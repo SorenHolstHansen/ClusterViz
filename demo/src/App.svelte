14be46f6639e5d7e8c835cd7df26f9b4b6767a57
sorenholsthansen <soerenhh@pm.me>
2022-03-11 18:18:14 +0100

    Packaging

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { ClusterViz, ClusterVizNode } from '@cluster-viz/core';
+	import { ClusterViz, ClusterVizNode } from 'cluster-viz';
 	import { onMount } from 'svelte';
 	import { data } from './data';
 
